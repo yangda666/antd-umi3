d1e368337eb1b717fdc6cbca186346678945a82a
coderYzj <215398186@qq.com>
2023-01-12 00:17:29 +0800

    feat: 升级antd家族

--- src/components/RightContent/index.tsx
+++ src/components/RightContent/index.tsx
@@ -1,28 +1,22 @@
 import { Space } from 'antd';
 import { QuestionCircleOutlined } from '@ant-design/icons';
 import React from 'react';
-import { useModel, SelectLang } from 'umi';
+import { useModel } from 'umi';
 import Avatar from './AvatarDropdown';
 import HeaderSearch from '../HeaderSearch';
 import styles from './index.less';
 import NoticeIconView from '../NoticeIcon';
-
 export type SiderTheme = 'light' | 'dark';
-
 const GlobalHeaderRight: React.FC = () => {
   const { initialState } = useModel('@@initialState');
-
   if (!initialState || !initialState.settings) {
     return null;
   }
-
   const { navTheme, layout } = initialState.settings;
   let className = styles.right;
-
   if ((navTheme === 'dark' && layout === 'top') || layout === 'mix') {
     className = `${styles.right}  ${styles.dark}`;
   }
-
   return (
     <Space className={className}>
       <HeaderSearch
@@ -60,9 +54,7 @@ const GlobalHeaderRight: React.FC = () => {
       </span>
       <NoticeIconView />
       <Avatar menu />
-      <SelectLang className={styles.action} />
     </Space>
   );
 };
-
 export default GlobalHeaderRight;
